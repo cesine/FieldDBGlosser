d6f217fda7a9bf3e67a824a59f6c0e6dc14b8c9a
cesine <cesine@yahoo.com>
2017-03-12 12:37:00 -0400

    updated build deps

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,12 +5,18 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
+   pkg: grunt.file.readJSON('package.json'),
     banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
       '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
       '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
       '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+      ' Licensed <%= pkg.licenses[0].type %> */\n',
+    // pkg: pkg,
+    // banner: '/*! '+ pkg.name + ' - v'+ pkg.version + ' - ' +
+    //   '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+    //    pkg.homepage ? pkg.homepage + "\\n" : "" +
+    //   '* Copyright (c) <%= grunt.template.today("yyyy") %> ' + pkg.author.name +
+    //   ' Licensed ' + _.pluck(pkg.licenses, "type").join(", ")+  ' */\n',
     // Task configuration.
     browserify: {
       src: {
